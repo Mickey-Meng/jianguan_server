8340c83a283619f4cd29593cb295203476b77396
zhangjing <13689271580zjj>
2022-12-07 14:39:33 +0800

    stearm

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/project/MeaLedgerBreakdownDetailController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/project/MeaLedgerBreakdownDetailController.java
@@ -99,7 +99,7 @@ public class MeaLedgerBreakdownDetailController extends BaseController {
             variables.put("fhsl",bo.getFhsl());
             variables.put("yjlsl",bo.getYjlsl());
             variables.put("fhje",bo.getFhje());
-            processService.startProcess("Process_1670392865296:1:0a26b6fe-75f5-11ed-9d0b-30c9aba7d4f3", variables);
+            processService.startProcess("Process_1670392865296:2:c9c6838c-75f9-11ed-9edd-30c9aba7d4f3", variables);
         }
         return toAjax(aBoolean ? 1 : 0);
     }
